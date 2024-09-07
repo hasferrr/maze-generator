31acbcba04acdffdf4c391ddb26c553b7f83c9ac
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-09-07 07:13:38 +0700

    feat: change grid colors

--- src/utils/generateClass.ts
+++ src/utils/generateClass.ts
@@ -1,10 +1,22 @@
-const GRID_COLOR = [
-  'bg-[#333333]',
-  'bg-cyan-500',
-  'bg-cyan-700',
-  'bg-emerald-400',
+import { twMerge } from 'tailwind-merge'
+
+enum KeyToColor {
+  wall = 'bg-slate-200',
+  path = 'bg-slate-950',
+  visited = 'bg-cyan-600',
+  result = 'bg-emerald-400',
+}
+
+const valueToKey: Array<keyof typeof KeyToColor> = [
+  'wall',
+  'path',
+  'visited',
+  'result',
 ]
 
-export const generateClass = (colorKey: number) => {
-  return `${GRID_COLOR[colorKey]}`
+export const generateClass = (n: number) => {
+  return twMerge(
+    KeyToColor[valueToKey[n]],
+    valueToKey[n] !== 'wall' && 'border-t border-r border-slate-200',
+  )
 }
