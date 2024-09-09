1808557f5c7614623e02c79307d86db6974a0cd9
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-09-09 21:05:06 +0700

    fix(algo): euclidean formula

--- src/utils/heuristics.ts
+++ src/utils/heuristics.ts
@@ -3,5 +3,5 @@ export const manhattanDistance = (x1: number, x2: number, y1: number, y2: number
 }
 
 export const euclideanDistance = (x1: number, x2: number, y1: number, y2: number): number => {
-  return (x1 - x2) ** 2 + (y1 - y2) ** 2
+  return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 }
