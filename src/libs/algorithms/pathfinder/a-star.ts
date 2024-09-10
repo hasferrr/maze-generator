8383493b2ad2ef8ce3702d8f2ad8d54c10e7fc66
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-09-10 08:48:17 +0700

    style(algo): improve var name and indents

--- src/libs/algorithms/pathfinder/a-star.ts
+++ src/libs/algorithms/pathfinder/a-star.ts
@@ -20,14 +20,13 @@ interface PosNode {
  * - end (100)
  */
 export const aStar = (grid: GridValues[][], start: PositionXY, end: PositionXY, type: HeuristicType,): StepListQueue => {
-  const steps: StepListQueue = new SinglyLinkedListQueue()
-
   const ROWS = grid.length
   const COLS = grid[0].length
   const DIRECTIONS = [
     [0, 1], [1, 0], [0, -1], [-1, 0]
   ]
 
+  const steps: StepListQueue = new SinglyLinkedListQueue()
   const previous = new Map<string, PositionXY | null>()
 
   const heuristicFn = type === 'manhattan'
@@ -41,6 +40,7 @@ export const aStar = (grid: GridValues[][], start: PositionXY, end: PositionXY, 
     return a.total - b.total
   })
 
+  previous.set(`${start[0]},${start[1]}`, null)
   const heuristicStart = heuristicFn(start[0], end[0], start[1], end[1])
   minHeap.insert({
     pos: start,
@@ -56,14 +56,8 @@ export const aStar = (grid: GridValues[][], start: PositionXY, end: PositionXY, 
       continue
     }
 
-    if (grid[x][y] === 1) {
-      steps.push([{ row: x, col: y, val: 2 }])
-    }
-    grid[x][y] = 2
-
     if (x === end[0] && y === end[1]) {
       const stepList: Step[] = []
-
       let backtrack = previous.get(`${x},${y}`)
       while (backtrack) {
         const [px, py] = backtrack
@@ -71,16 +65,19 @@ export const aStar = (grid: GridValues[][], start: PositionXY, end: PositionXY, 
         backtrack = previous.get(`${px},${py}`)
         stepList.push({ row: px, col: py, val: 3 })
       }
-
       for (let i = stepList.length - 2; i >= 0; i--) {
         steps.push([stepList[i]])
       }
-
+      steps.push([{ row: x, col: y, val: 3 }])
+      steps.push([{ row: end[0], col: end[1], val: 100 }])
       console.log(curr.cost)
       break
     }
 
-    for (const [dx, dy] of DIRECTIONS) {
+    steps.push([{ row: x, col: y, val: 2 }])
+    grid[x][y] = 2
+
+    for (const [dy, dx] of DIRECTIONS) {
       const nx = x + dx
       const ny = y + dy
       if (nx < 0 || ny < 0 || nx >= ROWS || ny >= COLS) {
@@ -101,6 +98,7 @@ export const aStar = (grid: GridValues[][], start: PositionXY, end: PositionXY, 
     }
   }
 
+  steps.shift()
   grid[start[0]][start[1]] = 99
   grid[end[0]][end[1]] = 100
 
--- src/libs/algorithms/pathfinder/bfs.ts
+++ src/libs/algorithms/pathfinder/bfs.ts
@@ -11,8 +11,6 @@ import { SinglyLinkedListQueue } from '../../datastructures/queue'
  * - end (100)
  */
 export const bfs = (grid: GridValues[][], start: PositionXY, end: PositionXY): StepListQueue => {
-  const steps: StepListQueue = new SinglyLinkedListQueue()
-
   const ROWS = grid.length
   const COLS = grid[0].length
   const DIRECTIONS = [
@@ -22,9 +20,11 @@ export const bfs = (grid: GridValues[][], start: PositionXY, end: PositionXY): S
   let shortest = 0
   let solved = false
 
-  const path = new Map<string, PositionXY | null>()
+  const steps: StepListQueue = new SinglyLinkedListQueue()
+  const previous = new Map<string, PositionXY | null>()
   const queue = new SinglyLinkedListQueue<PositionXY>()
-  path.set(`${start[0]},${start[1]}`, null)
+
+  previous.set(`${start[0]},${start[1]}`, null)
   queue.push(start)
 
   while (queue.length) {
@@ -32,40 +32,39 @@ export const bfs = (grid: GridValues[][], start: PositionXY, end: PositionXY): S
 
     for (let i = 0; i < qLen; i++) {
       const [x, y] = queue.shift()!
-      if (![1, 99, 100].includes(grid[x][y])) continue
+      if (![1, 99, 100].includes(grid[x][y])) {
+        continue
+      }
 
       if (x === end[0] && y === end[1]) {
-        const resultStepList: Step[] = []
-
-        let backtrack = path.get(`${x},${y}`)
+        const stepList: Step[] = []
+        let backtrack = previous.get(`${x},${y}`)
         while (backtrack) {
           const [px, py] = backtrack
           grid[px][py] = 3
-          backtrack = path.get(`${px},${py}`)
-          resultStepList.push({ row: px, col: py, val: 3 })
+          backtrack = previous.get(`${px},${py}`)
+          stepList.push({ row: px, col: py, val: 3 })
         }
-
-        for (let i = resultStepList.length - 2; i >= 0; i--) {
-          steps.push([resultStepList[i]])
+        for (let i = stepList.length - 2; i >= 0; i--) {
+          steps.push([stepList[i]])
         }
-
+        steps.push([{ row: x, col: y, val: 3 }])
+        steps.push([{ row: end[0], col: end[1], val: 100 }])
         solved = true
         break
       }
 
-      if (grid[x][y] === 1) {
-        steps.push([{ row: x, col: y, val: 2 }])
-      }
+      steps.push([{ row: x, col: y, val: 2 }])
       grid[x][y] = 2
 
       for (const [dy, dx] of DIRECTIONS) {
         const nx = x + dx
         const ny = y + dy
-        if (nx < 0 || ny < 0 || nx === ROWS || ny === COLS) {
+        if (nx < 0 || ny < 0 || nx >= ROWS || ny >= COLS) {
           continue
         }
         if ([1, 99, 100].includes(grid[nx][ny])) {
-          path.set(`${nx},${ny}`, [x, y])
+          previous.set(`${nx},${ny}`, [x, y])
           queue.push([nx, ny])
         }
       }
@@ -75,6 +74,7 @@ export const bfs = (grid: GridValues[][], start: PositionXY, end: PositionXY): S
     shortest++
   }
 
+  steps.shift()
   grid[start[0]][start[1]] = 99
   grid[end[0]][end[1]] = 100
 
